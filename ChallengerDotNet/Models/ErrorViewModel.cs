a8ecac0043a2af3e1580bce74dbd14b53a34ff40
alef <lafitealef@gmail.com>
2024-05-11 14:24:22 -0300

    Iniciando o projeto

---
+++ ChallengerDotNet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ChallengerDotNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
